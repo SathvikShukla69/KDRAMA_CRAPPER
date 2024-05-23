3d968f2972f6152f748ad4e97c1dee11c19f3151
SathvikShukla69 <itzanamikasrk@gmail.com>
2024-05-24 01:58:14 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,18 +2,18 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6336180921"
-    sudo_users = "6336180921"
-    GROUP_ID = -1002083632600
-    TOKEN = "7000408620:AAH7bxm-SgFc7r_UstXq-2PpeH-kT-URgDk"
+    OWNER_ID = "6714533496"
+    sudo_users = "6714533496"
+    GROUP_ID = -1002145832299
+    TOKEN = "7040663937:AAGkCNKfGuxJGcZNuPRpuHV25JPveEGPXIk"
     mongo_url = "mongodb+srv://Krishna:pss968048@cluster0.4rfuzro.mongodb.net/?retryWrites=true&w=majority"
-    PHOTO_URL = ["https://graph.org/file/b57bc44662b16d79b2130.jpg"]
-    SUPPORT_CHAT = "@BILLA_GANG_NTWK"
-    UPDATE_CHAT = "@BILLA_GANG_NTWK"
-    BOT_USERNAME = "@ReaLifeXWaifu_Bot"
-    CHARA_CHANNEL_ID = "-1002083632600"
-    api_id = 25355409
-    api_hash = "b9c741ba6b62f492dd0a3a39f7b2c526"
+    PHOTO_URL = ["https://graph.org/file/a8e50d3d65c77c2e61fe3.jpg"]
+    SUPPORT_CHAT = "https://t.me/BengaliSPY"
+    UPDATE_CHAT = "https://t.me/BengaliSPY"
+    BOT_USERNAME = "KdramaWaifu_Bot"
+    CHARA_CHANNEL_ID = "-1002145832299"
+    api_id = 26696109
+    api_hash = "c93b995a489c2637e2af1db49d305143"
 
     
 class Production(Config):
