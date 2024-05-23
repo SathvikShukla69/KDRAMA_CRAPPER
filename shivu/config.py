e7ca3a9f86718ed182e062dde1b5ac033bc2db85
Subham86589 <167736439+Subham86589@users.noreply.github.com>
2024-05-23 15:09:28 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -4,14 +4,14 @@ class Config(object):
     # Get this value from my.telegram.org/apps
     OWNER_ID = "6336180921"
     sudo_users = "6336180921"
-    GROUP_ID = -1002087144671
-    TOKEN = "6992957484:AAEVgdiLJLZ5qSgpWhOLAwmUOzdmEjcYZbY"
-    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
-    PHOTO_URL = ["https://telegra.ph/file/91285fe376f0994856ff8.jpg"]
+    GROUP_ID = -1002083632600
+    TOKEN = "7000408620:AAH7bxm-SgFc7r_UstXq-2PpeH-kT-URgDk"
+    mongo_url = "mongodb+srv://Krishna:pss968048@cluster0.4rfuzro.mongodb.net/?retryWrites=true&w=majority"
+    PHOTO_URL = ["https://graph.org/file/b57bc44662b16d79b2130.jpg"]
     SUPPORT_CHAT = "@BILLA_GANG_NTWK"
     UPDATE_CHAT = "@BILLA_GANG_NTWK"
-    BOT_USERNAME = "@FootballerXGrab_Bot"
-    CHARA_CHANNEL_ID = "-1002087144671"
+    BOT_USERNAME = "@ReaLifeXWaifu_Bot"
+    CHARA_CHANNEL_ID = "-1002083632600"
     api_id = 25355409
     api_hash = "b9c741ba6b62f492dd0a3a39f7b2c526"
 
