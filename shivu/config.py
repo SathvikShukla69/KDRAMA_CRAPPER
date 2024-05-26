f6dfa351c5af74dc8288a55bc2bcc13a5bc4d79a
SathvikShukla69 <itzanamikasrk@gmail.com>
2024-05-26 15:42:02 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -3,17 +3,17 @@ class Config(object):
 
     # Get this value from my.telegram.org/apps
     OWNER_ID = "6714533496"
-    sudo_users = "6714533496"
-    GROUP_ID = -1002242017124
-    TOKEN = "7040743526:AAEqa-H1wtmDUL7hrKXtURsiry4HVPpIUzE"
+    sudo_users = "6994941427"
+    GROUP_ID = -1001951112068
+    TOKEN = "7194498585:AAGbFU3VO8302x1Qo19w6kHqoW-ONdiLnIY"
     mongo_url = "mongodb+srv://Krishna:pss968048@cluster0.4rfuzro.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://graph.org/file/a8e50d3d65c77c2e61fe3.jpg"]
-    SUPPORT_CHAT = "https://t.me/Kdrama_all_hindi"
-    UPDATE_CHAT = "https://t.me/Kdrama_all_hindi"
+    SUPPORT_CHAT = "@Kdrama_all_hindi"
+    UPDATE_CHAT = "@Kdrama_all_hindi"
     BOT_USERNAME = "KdramaRealWaifu_Bot"
     CHARA_CHANNEL_ID = "-1916734111"
-    api_id = "26452652"
-    api_hash = "39656d6fe1fb40200126cefed1787ee8"
+    api_id = "26696109"
+    api_hash = "c93b995a489c2637e2af1db49d305143"
 
     
 class Production(Config):
